058f291b853425014c344bd289ece8bc70f729ff
Paul Nienaber <paul.nienaber@dremio.com>
2023-12-09 20:03:11 -0800

    [builds] Prelim Java implementation

--- java/flight/flight-core/src/main/java/org/apache/arrow/flight/FlightClient.java
+++ java/flight/flight-core/src/main/java/org/apache/arrow/flight/FlightClient.java
@@ -597,7 +597,7 @@ public class FlightClient implements AutoCloseable {
     SetSessionOptionsResult result;
     try {
       result = SetSessionOptionsResult.deserialize(ByteBuffer.wrap(results.next().getBody()));
-    } catch (IOException | URISyntaxException e) {
+    } catch (IOException e) {
       throw CallStatus.INTERNAL
           .withDescription("Failed to parse server response: " + e)
           .withCause(e)
